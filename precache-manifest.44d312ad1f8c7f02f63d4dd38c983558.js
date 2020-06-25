c098ade83a358787a9ee1a0c118abda2ae4c24c8
Jordan M <jordan.muliaman@gmail.com>
2020-06-25 16:11:37 -0700

    Updates

---
+++ precache-manifest.44d312ad1f8c7f02f63d4dd38c983558.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b951737fbd7af99eac57326aaef7d4a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "e1e5c18d2be9c5fc5d2f",
+    "url": "/React-Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cb00456a43b263e58121",
+    "url": "/React-Portfolio/static/css/main.55dfc51d.chunk.css"
+  },
+  {
+    "revision": "e1e5c18d2be9c5fc5d2f",
+    "url": "/React-Portfolio/static/js/2.7463af48.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.7463af48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb00456a43b263e58121",
+    "url": "/React-Portfolio/static/js/main.3b41870a.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "f83f530ae370b0c686ddfd6b42283f11",
+    "url": "/React-Portfolio/static/media/JM_logowhite.f83f530a.png"
+  },
+  {
+    "revision": "bc31c237893af0766d9567f0c8ff1a81",
+    "url": "/React-Portfolio/static/media/burger.bc31c237.JPG"
+  },
+  {
+    "revision": "00af51b69b206412d77442b843a5704e",
+    "url": "/React-Portfolio/static/media/comingsoon.00af51b6.JPG"
+  },
+  {
+    "revision": "8debe676a8db9b586d9e252b87bb2f22",
+    "url": "/React-Portfolio/static/media/dealtracker.8debe676.JPG"
+  },
+  {
+    "revision": "7bd5978f6335b635a7a6ca7a8a550f7c",
+    "url": "/React-Portfolio/static/media/greenland-1.7bd5978f.jpg"
+  },
+  {
+    "revision": "d3aae98a8f33c1baab374c8521ef17c2",
+    "url": "/React-Portfolio/static/media/greenland-2.d3aae98a.jpg"
+  },
+  {
+    "revision": "ef96bebc9c181e22f59f553f958e0b37",
+    "url": "/React-Portfolio/static/media/jordan-muliaman.ef96bebc.JPG"
+  },
+  {
+    "revision": "2674f7e9acb458ce875c14a9464708ae",
+    "url": "/React-Portfolio/static/media/myresume.2674f7e9.pdf"
+  },
+  {
+    "revision": "041257dc6645c9714a5f1b433819fc8a",
+    "url": "/React-Portfolio/static/media/pleasantbay.041257dc.jpg"
+  },
+  {
+    "revision": "8293422e36a47f3153d6177b5f71b621",
+    "url": "/React-Portfolio/static/media/pokemon.8293422e.JPG"
+  },
+  {
+    "revision": "4942b501b0bb9b6c56430411042cdd7b",
+    "url": "/React-Portfolio/static/media/resumelogo.4942b501.png"
+  }
+]);

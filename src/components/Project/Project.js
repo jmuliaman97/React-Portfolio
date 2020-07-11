793ab3c0ac9197e83e1b5d6313be54927ac8c4d3
Jordan M <jordan.muliaman@gmail.com>
2020-07-11 10:42:16 +0700

    edit project page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -21,9 +21,12 @@ const Project = () => {
           <Card color="dark" className="card">
             <CardImg className="cardImage" top width="100%" src={require('./images/pokemon.JPG')} alt="Pokemon Index" />
             <CardBody className="cardBody">
-              <CardTitle className="cardTitle">Pokémon Index</CardTitle>
+              <CardTitle>
+                <Button className="button" href="https://alanhlee.github.io/PokemonIndex/" color="dark">
+                  Pokémon Index
+                </Button>
+              </CardTitle>
               <CardText className="cardText">An application that shows the types, stats, weakness and strengths of the first 151 Pokémon.</CardText>
-              <Button href="https://alanhlee.github.io/PokemonIndex/" color="dark">Link to application</Button>
             </CardBody>
           </Card>
         </Col>
@@ -31,9 +34,12 @@ const Project = () => {
           <Card color="dark" className="card">
             <CardImg className="cardImage" top width="100%" src={require('./images/burger.JPG')} alt="Burger" />
             <CardBody className="cardBody">
-              <CardTitle className="cardTitle">Eat Da Burger!</CardTitle>
+              <CardTitle>
+                <Button className="button" href="https://damp-hamlet-57978.herokuapp.com/" color="dark">
+                  Eat Da Burger!
+                </Button>
+              </CardTitle>
               <CardText className="cardText">A restaurant app that lets users input the names of burgers they'd like to eat.</CardText>
-              <Button href="https://damp-hamlet-57978.herokuapp.com/" color="dark">Link to application</Button>
             </CardBody>
           </Card>
         </Col>
@@ -43,9 +49,12 @@ const Project = () => {
           <Card color="dark" className="card">
             <CardImg className="cardImage" top width="100%" src={require('./images/dealtracker.JPG')} alt="Deal Tracker" />
             <CardBody className="cardBody">
-              <CardTitle className="cardTitle">Deal Tracker</CardTitle>
+              <CardTitle>
+                <Button className="button" href="https://deal-tracker-app.herokuapp.com/" color="dark">
+                  Deal Tracker
+                </Button>
+              </CardTitle>
               <CardText className="cardText">An app that allows sales executives to create deals and move them through a visual pipeline at each stage in the sales process.</CardText>
-              <Button href="https://deal-tracker-app.herokuapp.com/" color="dark">Link to application</Button>
             </CardBody>
           </Card>
         </Col>
@@ -53,9 +62,12 @@ const Project = () => {
           <Card color="dark" className="card">
             <CardImg className="cardImage" top width="100%" src={require('./images/cache.JPG')} alt="CACHE" />
             <CardBody className="cardBody">
-              <CardTitle className="cardTitle">CACHE</CardTitle>
+              <CardTitle>
+                <Button className="button" href="https://uci-cache.herokuapp.com/" color="dark">
+                  C A C H E
+                </Button>
+              </CardTitle>
               <CardText className="cardText">An online sales, auction and bartering app. It combines the auction and buy-it-now features of eBay along with the open offer features of Craigslist.</CardText>
-              <Button href="google.com" color="dark">Link to application</Button>
             </CardBody>
           </Card>
         </Col>
